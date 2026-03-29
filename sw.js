0683dec6d28a2dfb9e3e600eb1414cc3d1063d9b
foor514 <orion526okumura@gmail.com>
2026-03-30 01:49:24 +0900

    キャッシュ

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'task-calendar-v1';
+const CACHE_NAME = 'task-calendar-v2';
 const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
