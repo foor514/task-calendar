58b4f791eee936c1c91538035444984fd0e85b5b
foor514 <orion526okumura@gmail.com>
2026-04-09 03:25:18 +0900

    ログイン認証追加

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'task-calendar-v6';
+const CACHE_NAME = 'task-calendar-v7';
 const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
